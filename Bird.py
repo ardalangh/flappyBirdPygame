8eea7d55a46709ed629e84c3ff60e9cfbd2c2e55
ardygh <ardygh@berkeley.edu>
2021-01-29 16:29:58 -0800

    end of the class on 1/29/2021

--- Bird.py
+++ Bird.py
@@ -10,3 +10,18 @@ class Bird(pygame.sprite.Sprite):
     SinkSpeed = 0.18
     ClimbSpeed = 0.3
     ClimbDuration = 333.3
+
+
+
+    # consturctor 
+    def __init__(self, x, y, secToClimb, images):
+        super(Bird, self).__init__()
+        self.x = x
+        self.y = y
+        self.secToClimb = secToClimb
+        self._img_wingup , self._img_wingdown = images
+        # mask them 
+        self._mask_wingup = pygame.mask.from_surface(self._img_wingup)
+        self._mask_wingdown = pygame.mask.from_surface(self._img_wingdown)
+
+    #  Stop here until next week 
---
+++ justForExploring/dictionaryExample.py
@@ -0,0 +1,19 @@
+"""
+    This file to explore and understand the methods of dictionary in python. 
+"""
+dict = {
+    "ardy": 23,
+    "mike": 10,
+    "sion": 11,
+    "faithlin": 12,
+}
+
+
+print(dict.values())
+# [23, 10, 12, 11]
+print(dict.keys())
+# ['ardy', 'mike', 'faithlin', 'sion']
+print(dict["ardy"])
+# 23
+print(len(dict))
+# 4
